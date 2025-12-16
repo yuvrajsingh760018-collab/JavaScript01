2f5631356e6ac9e89dff4514adfb4e10f32a7f22
yuvrajsingh760018-collab <yuvrajsingh760018@gmail.com>
2025-12-16 23:42:16 +0530

    nums_in_js

---
+++ 01_Basics/06_ nums_and_math.js
@@ -0,0 +1,9 @@
+const score = 400
+console.log(score)
+
+const balance  = new Number(100)
+console.log(balance)
+console.log("converting it into he string ");
+
+console.log(balance.toString()); // 100 (as a string 100)
+console.log(balance.toString().length);// length of the string 
